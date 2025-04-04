874e9b5aa7cd0dafee2e37a91e969c90d084e5bc
priyankahotkar <160253899+priyankahotkar@users.noreply.github.com>
2025-04-04 19:31:15 +0530

    Initial commit with project setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/NayanPortfolio/",  // ✅ Add your repo name here
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
